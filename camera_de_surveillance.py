c164a3a3bc1b0e1bbe1c8e721de1352c0a26da59
DriSidibe <you@example.com>
2023-09-29 09:01:08 +0000

    first commit

--- camera_de_surveillance.py
+++ camera_de_surveillance.py
@@ -4,112 +4,162 @@
 
 import cv2
 from datetime import datetime
-import os, ffmpeg
+
+cap = cv2.VideoCapture(0)
 
 
-def compress_video(video_full_path, output_file_name):
-    # Reference: https://en.wikipedia.org/wiki/Bit_rate#Encoding_bit_rate
-    min_audio_bitrate = 32000
-    max_audio_bitrate = 256000
+def make_1080p(camera):
+    camera.set(3, 1920)
+    camera.set(4, 1080)
 
-    probe = ffmpeg.probe(video_full_path)
-    # Video duration, in s.
-    duration = float(probe["format"]["duration"])
-    # Best min size, in kB.
-    best_min_size = (32000 + 100000) * (1.073741824 * duration) / (8 * 1024)
-    # Audio bitrate, in bps.
-    audio_bitrate = float(
-        next((s for s in probe["streams"] if s["codec_type"] == "audio"), None)[
-            "bit_rate"
-        ]
+
+def make_720p(camera):
+    camera.set(3, 1280)
+    camera.set(4, 720)
+
+
+def make_480p(camera):
+    camera.set(3, 640)
+    camera.set(4, 480)
+
+
+def change_res(camera, width, height):
+    camera.set(3, width)
+    camera.set(4, height)
+
+
+video_res = (160, 100)
+
+
+try:
+    # Create an object to read
+    # from camera
+    video = cv2.VideoCapture(0)
+
+    change_res(video, *video_res)
+    # change_res(1280, 720)
+
+    # We need to check if camera
+    # is opened previously or not
+    if video.isOpened() == False:
+        print("Error reading video file")
+
+    # We need to set resolutions.
+    # so, convert them from float to integer.
+    frame_width = int(video.get(3))
+    frame_height = int(video.get(4))
+
+    size = (frame_width, frame_height)
+
+    # Below VideoWriter object will create
+    # a frame of above defined The output
+    # is stored in 'filename.avi' file.
+    filename = "_".join(
+        "_".join(
+            "_".join("_".join(str(datetime.now()).split(" ")).split(":")).split("-")
+        ).split(".")
     )
-    # Target total bitrate, in bps.
-    target_total_bitrate = (best_min_size * 1024 * 8) / (1.073741824 * duration)
+    result = cv2.VideoWriter(
+        f"{filename}.avi", cv2.VideoWriter_fourcc(*"MJPG"), 5, size
+    )
 
-    # Target audio bitrate, in bps
-    if 10 * audio_bitrate > target_total_bitrate:
-        audio_bitrate = target_total_bitrate / 10
-        if audio_bitrate < min_audio_bitrate < target_total_bitrate:
-            audio_bitrate = min_audio_bitrate
-        elif audio_bitrate > max_audio_bitrate:
-            audio_bitrate = max_audio_bitrate
-    # Target video bitrate, in bps.
-    video_bitrate = target_total_bitrate - audio_bitrate
+    while True:
+        ret, frame = video.read()
 
-    i = ffmpeg.input(video_full_path)
-    ffmpeg.output(
-        i, os.devnull, **{"c:v": "libx264", "b:v": video_bitrate, "pass": 1, "f": "mp4"}
-    ).overwrite_output().run()
-    ffmpeg.output(
-        i,
-        output_file_name,
-        **{
-            "c:v": "libx264",
-            "b:v": video_bitrate,
-            "pass": 2,
-            "c:a": "aac",
-            "b:a": audio_bitrate,
-        },
-    ).overwrite_output().run()
+        if ret == True:
+            # Write the frame into the
+            # file 'filename.avi'
+            result.write(frame)
 
+            # Display the frame
+            # saved in the file
+            cv2.imshow("Frame", frame)
 
-# Create an object to read
-# from camera
-video = cv2.VideoCapture(0)
+            # Press S on keyboard
+            # to stop the process
+            if cv2.waitKey(1) & 0xFF == ord("s") or datetime.now().hour == 19:
+                break
 
-# We need to check if camera
-# is opened previously or not
-if video.isOpened() == False:
-    print("Error reading video file")
-
-# We need to set resolutions.
-# so, convert them from float to integer.
-frame_width = int(video.get(3))
-frame_height = int(video.get(4))
-
-size = (frame_width, frame_height)
-
-# Below VideoWriter object will create
-# a frame of above defined The output
-# is stored in 'filename.avi' file.
-filename = "_".join(
-    "_".join(
-        "_".join("_".join(str(datetime.now()).split(" ")).split(":")).split("-")
-    ).split(".")
-)
-result = cv2.VideoWriter(f"{filename}.avi", cv2.VideoWriter_fourcc(*"MJPG"), 10, size)
-
-while True:
-    ret, frame = video.read()
-
-    if ret == True:
-        # Write the frame into the
-        # file 'filename.avi'
-        result.write(frame)
-
-        # Display the frame
-        # saved in the file
-        cv2.imshow("Frame", frame)
-
-        # Press S on keyboard
-        # to stop the process
-        if cv2.waitKey(1) & 0xFF == ord("s") or datetime.now().hour == 19:
+        # Break the loop
+        else:
             break
 
-    # Break the loop
-    else:
-        break
+    # When everything done, release
+    # the video capture and video
+    # write objects
+    video.release()
+    result.release()
 
-# When everything done, release
-# the video capture and video
-# write objects
-video.release()
-result.release()
+    # Closes all the frames
+    cv2.destroyAllWindows()
 
-# Closes all the frames
-cv2.destroyAllWindows()
+    # Compress input.avi to 50MB and save as output.avi
+    # compress_video(f"C:\\Users\\dsidi.DARKER\\{filename}.avi", f"{filename}.avi")
 
-# Compress input.avi to 50MB and save as output.avi
-# compress_video(f"C:\\Users\\dsidi.DARKER\\{filename}.avi", f"{filename}.avi")
+    print(f"The video {filename} was successfully saved")
+except:
+    # Create an object to read
+    # from camera
+    video = cv2.VideoCapture(0)
 
-print("The video was successfully saved")
+    change_res(video, *video_res)
+    # change_res(1280, 720)
+
+    # We need to check if camera
+    # is opened previously or not
+    if video.isOpened() == False:
+        print("Error reading video file")
+
+    # We need to set resolutions.
+    # so, convert them from float to integer.
+    frame_width = int(video.get(3))
+    frame_height = int(video.get(4))
+
+    size = (frame_width, frame_height)
+
+    # Below VideoWriter object will create
+    # a frame of above defined The output
+    # is stored in 'filename.avi' file.
+    filename = "_".join(
+        "_".join(
+            "_".join("_".join(str(datetime.now()).split(" ")).split(":")).split("-")
+        ).split(".")
+    )
+    result = cv2.VideoWriter(
+        f"{filename}.avi", cv2.VideoWriter_fourcc(*"MJPG"), 5, size
+    )
+
+    while True:
+        ret, frame = video.read()
+
+        if ret == True:
+            # Write the frame into the
+            # file 'filename.avi'
+            result.write(frame)
+
+            # Display the frame
+            # saved in the file
+            cv2.imshow("Frame", frame)
+
+            # Press S on keyboard
+            # to stop the process
+            if cv2.waitKey(1) & 0xFF == ord("s") or datetime.now().hour == 19:
+                break
+
+        # Break the loop
+        else:
+            break
+
+    # When everything done, release
+    # the video capture and video
+    # write objects
+    video.release()
+    result.release()
+
+    # Closes all the frames
+    cv2.destroyAllWindows()
+
+    # Compress input.avi to 50MB and save as output.avi
+    # compress_video(f"C:\\Users\\dsidi.DARKER\\{filename}.avi", f"{filename}.avi")
+
+    print(f"The video {filename} was successfully saved")
